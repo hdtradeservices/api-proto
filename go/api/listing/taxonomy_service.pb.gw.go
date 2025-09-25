fe8a8f20542ba8a7c5abcc5281c60d33d29a3152
Matt <matt@zentail.com>
2025-09-25 13:38:01 -0400

    feat(listing): ListAttributeSpecs
    fixes: DEV-1954

--- go/api/listing/taxonomy_service.pb.gw.go
+++ go/api/listing/taxonomy_service.pb.gw.go
@@ -207,6 +207,58 @@ func local_request_TaxonomyService_AttributeSpec_0(ctx context.Context, marshale
 
 }
 
+func request_TaxonomyService_ListAttributeSpecs_0(ctx context.Context, marshaler runtime.Marshaler, client TaxonomyServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListAttributeSpecsRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["product_type_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "product_type_id")
+	}
+
+	protoReq.ProductTypeId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "product_type_id", err)
+	}
+
+	msg, err := client.ListAttributeSpecs(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_TaxonomyService_ListAttributeSpecs_0(ctx context.Context, marshaler runtime.Marshaler, server TaxonomyServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq ListAttributeSpecsRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["product_type_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "product_type_id")
+	}
+
+	protoReq.ProductTypeId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "product_type_id", err)
+	}
+
+	msg, err := server.ListAttributeSpecs(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 func request_TaxonomyService_CreateAttributeSpec_0(ctx context.Context, marshaler runtime.Marshaler, client TaxonomyServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq CreateAttributeSpecRequest
 	var metadata runtime.ServerMetadata
@@ -513,6 +565,29 @@ func RegisterTaxonomyServiceHandlerServer(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("GET", pattern_TaxonomyService_ListAttributeSpecs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/listing_api.TaxonomyService/ListAttributeSpecs", runtime.WithHTTPPathPattern("/v2/storefront/taxonomy/attribute_spec/{product_type_id}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TaxonomyService_ListAttributeSpecs_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TaxonomyService_ListAttributeSpecs_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_TaxonomyService_CreateAttributeSpec_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -703,6 +778,26 @@ func RegisterTaxonomyServiceHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("GET", pattern_TaxonomyService_ListAttributeSpecs_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/listing_api.TaxonomyService/ListAttributeSpecs", runtime.WithHTTPPathPattern("/v2/storefront/taxonomy/attribute_spec/{product_type_id}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TaxonomyService_ListAttributeSpecs_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TaxonomyService_ListAttributeSpecs_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("POST", pattern_TaxonomyService_CreateAttributeSpec_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -775,6 +870,8 @@ var (
 
 	pattern_TaxonomyService_AttributeSpec_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4, 1, 0, 4, 1, 5, 5}, []string{"v2", "storefront", "taxonomy", "attribute_spec", "product_type_id", "attribute_spec_id"}, ""))
 
+	pattern_TaxonomyService_ListAttributeSpecs_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"v2", "storefront", "taxonomy", "attribute_spec", "product_type_id"}, ""))
+
 	pattern_TaxonomyService_CreateAttributeSpec_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"v2", "storefront", "taxonomy", "attribute_spec", "product_type_id"}, ""))
 
 	pattern_TaxonomyService_UpdateAttributeSpec_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"v2", "storefront", "taxonomy", "attribute_spec", "product_type_id"}, ""))
@@ -790,6 +887,8 @@ var (
 	forward_TaxonomyService_DeleteProductType_0 = runtime.ForwardResponseMessage
 
 	forward_TaxonomyService_AttributeSpec_0 = runtime.ForwardResponseMessage
+
+	forward_TaxonomyService_ListAttributeSpecs_0 = runtime.ForwardResponseMessage
 
 	forward_TaxonomyService_CreateAttributeSpec_0 = runtime.ForwardResponseMessage
 
