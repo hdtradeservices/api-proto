cdeb82dc52f59285bd8ae53691bf906de0908f36
Matt <matt@zentail.com>
2026-03-13 12:00:04 -0400

    fix(listing): Put StorefrontStatus in the correct service

--- go/api/listing/sales_channel_service.pb.gw.go
+++ go/api/listing/sales_channel_service.pb.gw.go
@@ -65,6 +65,58 @@ func local_request_SalesChannelService_InitiateIngestion_0(ctx context.Context, 
 
 }
 
+func request_SalesChannelService_StorefrontStatus_0(ctx context.Context, marshaler runtime.Marshaler, client SalesChannelServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq StorefrontStatusRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["storefront_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "storefront_id")
+	}
+
+	protoReq.StorefrontId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "storefront_id", err)
+	}
+
+	msg, err := client.StorefrontStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_SalesChannelService_StorefrontStatus_0(ctx context.Context, marshaler runtime.Marshaler, server SalesChannelServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq StorefrontStatusRequest
+	var metadata runtime.ServerMetadata
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["storefront_id"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "storefront_id")
+	}
+
+	protoReq.StorefrontId, err = runtime.String(val)
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "storefront_id", err)
+	}
+
+	msg, err := server.StorefrontStatus(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterSalesChannelServiceHandlerServer registers the http handlers for service SalesChannelService to "mux".
 // UnaryRPC     :call SalesChannelServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -91,6 +143,29 @@ func RegisterSalesChannelServiceHandlerServer(ctx context.Context, mux *runtime.
 		}
 
 		forward_SalesChannelService_InitiateIngestion_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("GET", pattern_SalesChannelService_StorefrontStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req, "/listing_api.SalesChannelService/StorefrontStatus", runtime.WithHTTPPathPattern("/v2/storefront/status/{storefront_id}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_SalesChannelService_StorefrontStatus_0(rctx, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SalesChannelService_StorefrontStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -155,13 +230,37 @@ func RegisterSalesChannelServiceHandlerClient(ctx context.Context, mux *runtime.
 
 	})
 
+	mux.Handle("GET", pattern_SalesChannelService_StorefrontStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req, "/listing_api.SalesChannelService/StorefrontStatus", runtime.WithHTTPPathPattern("/v2/storefront/status/{storefront_id}"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_SalesChannelService_StorefrontStatus_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_SalesChannelService_StorefrontStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
 var (
 	pattern_SalesChannelService_InitiateIngestion_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"ingestion", "initiate"}, ""))
+
+	pattern_SalesChannelService_StorefrontStatus_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"v2", "storefront", "status", "storefront_id"}, ""))
 )
 
 var (
 	forward_SalesChannelService_InitiateIngestion_0 = runtime.ForwardResponseMessage
+
+	forward_SalesChannelService_StorefrontStatus_0 = runtime.ForwardResponseMessage
 )
